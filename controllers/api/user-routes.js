3b9afcbb760a98604d5876ab31035fb13bac84c3
Mark Pavel <markpavel02@gmail.com>
2022-06-27 18:44:16 -0400

    Added Single-Page Post

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -110,6 +110,7 @@ router.post('/login', (req, res) => {
 });
 
 router.post('/logout', (req, res) => {
+    console.log(req.session)
     if (req.session.loggedIn) {
         req.session.destroy(() => {
             res.status(204).end();
